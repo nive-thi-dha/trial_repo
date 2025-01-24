1263a43e7d95a597806cd86307558eb62300a9e2
nive-thi-dha <Nivethidha.S@gds.ey.com>
2025-01-24 12:03:21 +0530

    Add files via upload

---
+++ migrations/simple_integration.sh
@@ -0,0 +1,21 @@
+# simple_integration.sh script that reads workflow.conf file and execute it.
+#!/bin/bash
+# author: Marcel Castro
+set -e
+print_log () {
+    printf "[`date +'%d/%m/%Y %H:%M:%S'`] [$1] $2\n"
+}
+export -f print_log
+
+run_workflow () {
+    print_log "INFO" "Running workflow"
+    workflow_config=$(echo sed -e 's/#.*$//' -e '/^$/d' workflow.conf)
+    while IFS= read -r sql_file_name; do
+        print_log "INFO" "Running: $sql_file_name"
+        print_log "INFO" "Running ~/snowflake/snowsql -a $SF_ACCOUNT -u $SF_USERNAME -r $SF_ROLE -w $SF_WAREHOUSE -d $SF_DATABASE --private-key-path $SNOWFLAKE_PRIVATE_KEY_PATH -f ${GITHUB_WORKSPACE}/${sql_file_name}"
+        ~/snowflake/snowsql -a $SF_ACCOUNT -u $SF_USERNAME -r $SF_ROLE -w $SF_WAREHOUSE -d $SF_DATABASE --private-key-path $SNOWFLAKE_PRIVATE_KEY_PATH -f ${GITHUB_WORKSPACE}/${sql_file_name}
+    done < <($workflow_config);
+}
+
+## running workflow
+run_workflow
