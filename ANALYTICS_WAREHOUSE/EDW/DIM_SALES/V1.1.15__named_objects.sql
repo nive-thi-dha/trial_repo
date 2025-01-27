85f9bbb712acd828117e1089cf49d1ce4a0d25c1
nive-thi-dha <Nivethidha.S@gds.ey.com>
2025-01-27 15:37:35 +0530

    Create V1.1.15__named_objects.sql

---
+++ ANALYTICS_WAREHOUSE/EDW/DIM_SALES/V1.1.15__named_objects.sql
@@ -0,0 +1,13 @@
+USE DATABASE EDW;
+CREATE SCHEMA DIM_SALES;
+CREATE TABLE FACT_ACCOUNT_SALES_SNAP
+(
+    SalesID INT,
+    DateKey DATE NOT NULL,
+    ProductKey INT NOT NULL,
+    CustomerKey INT NOT NULL,
+    StoreKey INT NOT NULL,
+    SalesAmount DECIMAL(10, 2) NOT NULL,
+    Quantity INT NOT NULL,
+    Discount DECIMAL(5, 2)
+);
