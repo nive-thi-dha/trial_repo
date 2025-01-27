3da1c2aef3466b3a7ba2eb96233b171e30ebcede
nive-thi-dha <Nivethidha.S@gds.ey.com>
2025-01-27 11:23:16 +0530

    Update create_clone.py

--- migrations/create_clone.py
+++ migrations/create_clone.py
@@ -34,15 +34,10 @@ def restore_snowflake_db(backup_db, target_db, user, password, account, warehous
 		# SQL command to create a clone of the backup database
         clone_command = f"CREATE DATABASE DB_BKP CLONE DB;"
 		
-		# SQL command to create a table named secondtable
+		# SQL command to create a table named my_table
         sql_command = f"USE SCHEMA DEMO;"
-	create_table_sql = """
-        CREATE OR REPLACE TABLE my_table (
-            id INT,
-            name STRING,
-            created_at TIMESTAMP
-        );
-        """
+	create_table_sql = f"CREATE OR REPLACE TABLE my_table (id INT,name STRING,created_at TIMESTAMP);"
+	    
         except Exception as e:
         print(f"Error during deployment: {e}")
         sys.restore_command()
